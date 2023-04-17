6e58d203ab7c10370f2742bc08f6122781768d5b
Jeff Nelson <jpnelson85@gmail.com>
2023-04-17 10:06:24 -0500

    item test spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,1 +1,17 @@
-require 'rspec'
+require './lib/item'
+
+RSpec.describe Item do
+  it 'exists' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    expect(item1).to be_a(Item)
+    expect(item2).to be_a(Item)
+  end
+
+  it 'has attributes' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    expect(item2.name).to eq("Tomato")
+    expect(item2.price).to eq(.5)
+  end
+end
