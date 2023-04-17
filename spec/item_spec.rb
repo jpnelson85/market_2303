621bf01a8e6952b459373bfd26c719abdf67fbec
Jeff Nelson <jpnelson85@gmail.com>
2023-04-17 10:14:51 -0500

    add vendor spec tests

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -12,6 +12,6 @@ RSpec.describe Item do
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
     expect(item2.name).to eq("Tomato")
-    expect(item2.price).to eq(.5)
+    expect(item2.price).to eq(0.5)
   end
 end
---
+++ spec/vendor_spec.rb
@@ -0,0 +1,32 @@
+require './lib/item'
+require './lib/vendor'
+
+RSpec.describe Vendor do
+  it 'exists' do
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    expect(vendor).to be_a(Vendor)
+  end
+
+  it 'has a name and array of inventory and check stock' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    expect(vendor.name).to eq("Rocky Mountain Fresh")
+    expect(vendor.inventory).to eq({})
+    expect(vendor.check_stock(item1)).to eq(0)
+  end
+
+  it 'can stock inventory' do
+    item1 = Item.new({name: 'Peach', price: "$0.75"})
+    item2 = Item.new({name: 'Tomato', price: '$0.50'})
+    vendor = Vendor.new("Rocky Mountain Fresh")
+    expect(vendor.check_stock(item1)).to eq(0)
+    vendor.stock(item1, 30)
+    expect(vendor.inventory).to eq({item1 => 30})
+    expect(vendor.check_stock(item1)).to eq(30)
+    vendor.stock(item1, 25)
+    expect(vendor.check_stock(item1)).to eq(55)
+    vendor.stock(item2, 12)
+    expect(vendor.inventory).to eq({item1 => 55, item2 => 12})
+  end
+end
